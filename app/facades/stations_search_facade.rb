a07ceba6ded4a2d87a037f9b1b419d2ecb863477
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-26 10:46:48 -0600

    Add Station model and rename facade

--- app/facades/stations_search_facade.rb
+++ app/facades/stations_search_facade.rb
@@ -1,4 +1,4 @@
-class SearchStationsFacade
+class StationsSearchFacade
   def initialize(zip_code)
     @zip_code = zip_code
   end
@@ -8,6 +8,14 @@ class SearchStationsFacade
       f.headers["X-Api-Key"] = ENV["NREL_API_KEY"]
       f.params["limit"] = 10
       f.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?fuel_type=ELEC,LPG&location=#{@zip_code}&radius=6.0&access=public")
+
+    station_search_info = JSON.parse(response.body, symbolize_names: true)
+
+    station_search_info.map do |station_info|
+      Station.new(station_info)
     end
   end
 end

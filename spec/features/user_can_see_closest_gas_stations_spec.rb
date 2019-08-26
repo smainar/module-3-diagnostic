20e10c0c401dc54bfd8675fc0f380ba19595e6c9
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-26 11:00:11 -0600

    Delete comments from feature test

--- spec/features/user_can_see_closest_gas_stations_spec.rb
+++ spec/features/user_can_see_closest_gas_stations_spec.rb
@@ -1,14 +1,5 @@
 require "rails_helper"
 
-# As a user
-# When I visit "/"
-# And I fill in the search form with 80203 (Note: Use the existing search form)
-# And I click "Locate"
-# Then I should be on page "/search"
-# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-# And the stations should be limited to Electric and Propane
-# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
-
 RSpec.describe "As a user", type: :feature do
   describe "Search Form" do
     it "I should see a list of the 10 closest stations within 6 miles sorted by distance" do

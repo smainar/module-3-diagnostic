f3fdcbee337cb678957d1db819c06bc13970bacb
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-26 09:45:52 -0600

    Update sqlite gem to version 1.3.6

--- spec/features/user_can_see_closest_gas_stations_spec.rb
+++ spec/features/user_can_see_closest_gas_stations_spec.rb
@@ -14,13 +14,15 @@ RSpec.describe "As a user", type: :feature do
     it "I should see a list of the 10 closest stations within 6 miles sorted by distance" do
       visit "/"
 
-      fill_in "Zip", with: 80203
-      click_on "Locate"
+      within (".navbar") do
+        fill_in "Zip", with: 80203
+        click_on "Locate"
+      end
 
       expect(current_path).to eq("/search")
 
       expect(page).to have_css(".closest-stations", count: 10)
-      
+
       within (first(".closest-stations")) do
         expect(page).to have_content("Name")
         expect(page).to have_content("Address")

fb7b5d36c17c43406ed081cd43dd2e69be98e932
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-26 10:35:27 -0600

    Add stations method in StationsSearchFacade and update index method in SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-    
+    render locals: {
+      facade: StationsSearchFacade.new(params[:q])
+    }
   end
 end
--- app/facades/stations_search_facade.rb
+++ app/facades/stations_search_facade.rb
@@ -2,4 +2,12 @@ class SearchStationsFacade
   def initialize(zip_code)
     @zip_code = zip_code
   end
+
+  def stations
+    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json") do |f|
+      f.headers["X-Api-Key"] = ENV["NREL_API_KEY"]
+      f.params["limit"] = 10
+      f.adapter Faraday.default_adapter
+    end
+  end
 end
